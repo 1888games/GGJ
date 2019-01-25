61b26439a82acb9d6e3b4c510edbd73fc4b8a6f1
Steve Hall <steve@oclipa.com>
2019-01-25 22:57:34 +0000

    Add ToddlerController (and related scripts)

---
+++ Assets/SteveScripts/Commands/Command.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command
+{
+    public abstract void Execute(ToddlerController toddlerController);
+
+    protected void changeDirection(ToddlerController toddlerController, Vector3 direction)
+    {
+        if (!toddlerController.CurrentDirections.Contains(direction))
+            toddlerController.CurrentDirections.Add(direction);
+    }
+}
