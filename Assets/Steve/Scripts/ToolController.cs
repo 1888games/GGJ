b8d351658cc64a8612e4b6ad2899bf570c6dfd42
Steve Hall <steve@oclipa.com>
2019-01-26 14:19:31 +0000

    Add ability for tools to play audio when being held

--- Assets/Steve/Scripts/ToolController.cs
+++ Assets/Steve/Scripts/ToolController.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class ToolController : MonoBehaviour
 {
     [SerializeField]
-    private bool isNoisy;
+    private AudioClip audioClip;
 
     AudioSource audio;
     MeshRenderer meshRenderer;
+
+    bool audioPlaying;
 
     // Start is called before the first frame update
     void Start()
     {
         audio = GetComponent<AudioSource>();
+        if (audioClip != null)
+            audio.clip = audioClip;
         meshRenderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (IsHeld && audioClip != null && !audioPlaying)
+        {
+            audioPlaying = true;
+            audio.Play();
+        }
+        else if (!IsHeld && audioPlaying)
+        {
+            audioPlaying = false;
+            audio.Stop();
+        }
     }
 
     public bool IsHeld
@@ -32,6 +46,14 @@ public class ToolController : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             meshRenderer.material.color = Color.red;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            meshRenderer.material.color = Color.white;
         }
     }
 }
