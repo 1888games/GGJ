38299197ed5aeab056a7194ce2f2d0fbfb17d7e9
Steve Hall <steve@oclipa.com>
2019-01-26 19:01:16 +0000

    Add Socket

---
+++ Assets/Steve/Scripts/Socket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Socket : AbstractTarget
+{
+    public override bool TryReact()
+    {
+        if (_itemsIWillReactWith.Contains(ToddlerController.CurrentTool))
+        {
+            // play sound
+            // play particle effects
+            return true;
+        }
+        else
+        {
+            // no reaction.
+            return false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
