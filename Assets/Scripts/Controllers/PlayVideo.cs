623cdd01bb3a9e3d8a7283a98e8b911dfd49b675
Nick Sherman <nick@1888games.co.uk>
2019-01-26 17:28:04 +0000

    Nick UI

---
+++ Assets/Scripts/Controllers/PlayVideo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour
+{
+   //public UnityEngine.Video.VideoClip videoClip;
+
+    void Start()
+    {
+		var videoPlayer = GetComponent<VideoPlayer> ();
+        //var audioSource = gameObject.AddComponent<AudioSource>();
+
+		videoPlayer.playOnAwake = false;
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
+        videoPlayer.targetMaterialProperty = "_MainTex";
+        //videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
+        //videoPlayer.SetTargetAudioSource(0, audioSource);
+
+		videoPlayer.Play ();
+    }
+    
+    
+}
