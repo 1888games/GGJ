59c92dd881ade746ee2065b3e81b6d46fe93c48e
Nick Sherman <nick@1888games.co.uk>
2019-01-26 10:58:25 +0000

    Switch rotation to camera rather than world.

--- Assets/ControlBoard.cs
+++ Assets/ControlBoard.cs
@@ -8,11 +8,15 @@ public class ControlBoard : MonoBehaviour
 {
 
 	public float rotateSpeed = 0.05f;
-	public float minRotation = -40f;
-	public float maxRotation = 100f;
+	public float minRotation = -50f;
+	public float maxRotation = 50f;
 	float mult = 0f;
 	float newY;
 	float oldY;
+
+	float currentRotateSpeed = 0f;
+
+	public float damping = 0.05f;
 
 	
     // Start is called before the first frame update
@@ -24,15 +28,22 @@ public class ControlBoard : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
 
-		mult = 0f;
+		//mult = 0f;
 		
 		if (Input.GetKey (KeyCode.Comma)) {
 			mult = -1f;
 		}
 
-
 		if (Input.GetKey (KeyCode.Period)) {
 			mult = 1f;
+		}
+
+		if (mult > 0f) {
+			mult = Mathf.Clamp(mult - damping,0f,mult);
+		}
+		
+		if (mult < 0f) {
+			mult = Mathf.Clamp(mult + damping,mult,0f);
 		}
 
 		if (mult != 0f) {
