71a1cd7467840c23971182fdaceb347b117e8370
Nick Sherman <nick@1888games.co.uk>
2019-01-26 15:53:55 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/CullShadows.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+ using System.Collections;
+ 
+ public class CullShadows : MonoBehaviour {
+ 
+     public Shader unlitShader;
+ 
+     void Start() {
+       //  unlitShader = Shader.Find("Unlit/Texture");
+       //  GetComponent<Camera>().SetReplacementShader(unlitShader,"");
+     }
+     
+     private float previousShadowDistance;
+      
+    void OnPreRender () {
+        previousShadowDistance = QualitySettings.shadowDistance;
+        QualitySettings.shadowDistance = 0;
+    }
+    void OnPostRender(){
+        QualitySettings.shadowDistance = previousShadowDistance;
+    }
+ }
