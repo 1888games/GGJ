2106a4237e7d8ad0d6f0d635b90718308b9dfbcb
Nick Sherman <nick@1888games.co.uk>
2019-01-27 09:21:14 +0000

    Procrastinatory camera zoom

--- Assets/Steve/Scripts/CameraFollow.cs
+++ Assets/Steve/Scripts/CameraFollow.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -9,6 +10,16 @@ public class CameraFollow : MonoBehaviour
 
     private Vector3 offset;
 
+	public float zoomOutSize = 5f;
+	public float zoomInSize = 2.75f;
+
+	public float targetSize = 2.75f;
+
+	public float sizePerFrame = 0.01f;
+
+	public Vector3 lastPlayerPosition;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +27,41 @@ public class CameraFollow : MonoBehaviour
 
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - player.transform.position;
+
+		targetSize = zoomInSize;
+		Camera.main.orthographicSize = zoomOutSize;
+
+		lastPlayerPosition = player.transform.position;
+		
     }
 
+	void Update () {
+
+		Camera c = Camera.main;
+
+		if (lastPlayerPosition != player.transform.position) {
+			targetSize = zoomOutSize;
+		} else {
+			targetSize = zoomInSize;
+		}
+
+		if (c.orthographicSize > targetSize) {
+			c.orthographicSize = Mathf.Clamp (c.orthographicSize - sizePerFrame, targetSize, c.orthographicSize);
+		}
+
+		if (c.orthographicSize < targetSize) {
+			c.orthographicSize = Mathf.Clamp (c.orthographicSize + sizePerFrame, c.orthographicSize, targetSize);
+		}
+
+		lastPlayerPosition = player.transform.position;
+
+
+	}
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         transform.position = player.transform.position + offset;
+
     }
 }
