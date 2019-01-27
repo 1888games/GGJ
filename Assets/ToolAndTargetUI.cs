b96567e03ca036f404d5a2217205709657634c63
Nick Sherman <nick@1888games.co.uk>
2019-01-27 14:44:33 +0000

    Fix Interaction UI

--- Assets/ToolAndTargetUI.cs
+++ Assets/ToolAndTargetUI.cs
@@ -25,10 +25,15 @@ public class ToolAndTargetUI : MonoBehaviour {
 		ToddlerController.OnDropped += OnDroppedObject;
 		ToddlerController.PickupAttempt += OnAttemptPickup;
 		ToddlerController.OnWalkaway += OnCancelPickup;
+
+		ToddlerController.OnChaseModeFinished += OnChaseFinished;
+		
 		
 
 		AbstractTarget.InteractionAttempt += OnApproachObject;
 		AbstractTarget.OnWalkaway += OnCancelInteract;
+		AbstractTarget.ConfirmationReceived += CancelAll;
+	
 		
 		
     }
@@ -43,7 +48,18 @@ public class ToolAndTargetUI : MonoBehaviour {
         OnPickedUpObject (null);
  
     }
- 
+
+	void OnChaseFinished () {
+
+		spaceText.DOFade (0f, 1f);
+		carryingBar.DOFade (0f, 1f);
+		targetBar.DOFade (0f, 1f);
+        plusBar.DOFade (0f, 1f);
+
+
+	}
+	
+	
     [Button ("test drop")]
     void TestDrop () {
  
@@ -96,10 +112,19 @@ public class ToolAndTargetUI : MonoBehaviour {
 	void OnCancelInteract (AbstractTool tool, AbstractTarget target) {
 			
 		spaceText.DOFade (0f, 1f);
+		targetBar.DOFade (0f, 1f);
+		plusBar.DOFade (0f, 1f);
+      
+	}
+
+
+	void CancelAll (AbstractTool tool, AbstractTarget target) {
+
+		spaceText.DOFade (0f, 1f);
 		carryingBar.DOFade (0f, 1f);
 		targetBar.DOFade (0f, 1f);
-        spaceText.DOFade (0f, 1f);
-		
+        plusBar.DOFade (0f, 1f);
+
 	}
  
     void OnPickedUpObject (AbstractTool tool) {
