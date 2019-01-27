268ee8575bc5484b42894aaa9d473b81c42ffa45
Nick Sherman <nick@1888games.co.uk>
2019-01-27 14:23:12 +0000

    Added TV controller

---
+++ Assets/Scripts/Controllers/TVController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviourSingleton<PlayVideo>
+{
+	//public UnityEngine.Video.VideoClip videoClip;
+
+	bool tvOn = false;
+	public AudioSource music;
+
+	VideoPlayer videoPlayer;
+
+	void Awake () {
+		
+		music.playOnAwake = false;
+	}
+
+    void Start()
+    {
+		videoPlayer = GetComponent<VideoPlayer> ();
+        //var audioSource = gameObject.AddComponent<AudioSource>();
+
+		videoPlayer.playOnAwake = false;
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
+        videoPlayer.targetMaterialProperty = "_MainTex";
+		//videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
+		//videoPlayer.SetTargetAudioSource(0, audioSource);
+
+		music.playOnAwake = false;
+		music.Stop ();
+
+		//videoPlayer.Play ();
+    }
+
+	public bool ToggleTV () {
+
+		tvOn = !tvOn;
+
+		if (tvOn) {
+			videoPlayer.Play ();
+			music.Play ();
+		} else {
+			videoPlayer.Stop ();
+			music.Stop ();
+		}
+
+		return tvOn;
+		
+	}
+    
+}
