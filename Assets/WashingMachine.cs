88620e8c7f5d592412a8b3bba515001b8151e91d
Steve Hall <steve@oclipa.com>
2019-01-27 13:38:03 +0000

    Add washing machine script

---
+++ Assets/WashingMachine.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WashingMachine : AbstractTarget
+{
+    [SerializeField] GameObject _noisePrefab;
+
+    public override void React()
+    {
+        if (IsReactableTool())
+        {
+            Wash();
+        }
+        else
+        {
+            // do nothing
+        }
+    }
+
+    private void Wash()
+    {
+        print("Washing machine success reaction.");
+
+        ToddlerController.Instance.DestroyTool();
+
+        // TODO: Need correct sound for washing machine 
+
+        Animation animation = GetComponent<Animation>();
+        if (animation != null)
+            animation.Play();
+        Fabric.EventManager.Instance.PostEvent("Broken_Blender", Fabric.EventAction.PlaySound, null, gameObject);
+
+        ToddlerController.Instance.OnMadeNoise();
+    }
+}
